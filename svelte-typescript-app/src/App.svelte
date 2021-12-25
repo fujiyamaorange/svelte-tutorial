910d71524fbc9851e977f662cc39f62bfb925813
FujimuraKaito <kaito.bakuman@gmail.com>
2021-12-25 22:29:31 +0900

    🔥 remove route

--- svelte-typescript-app/src/App.svelte
+++ svelte-typescript-app/src/App.svelte
@@ -3,19 +3,22 @@
   import Top from './pages/Top.svelte'
   import Todo from './pages/Todo.svelte'
   import Video from './pages/Video.svelte'
+
+  export let url = ''
 </script>
 
-<Router>
+<Router {url}>
   <nav>
     <!-- display all time -->
-    <Link to="">home page |</Link>
-    <Link to="top">top page |</Link>
+    <Link to="/">home page |</Link>
     <Link to="todo">todo page |</Link>
     <Link to="video">video page</Link>
   </nav>
   <main>
-    <Route path="top" component={Top} />
     <Route path="todo" component={Todo} />
     <Route path="video" component={Video} />
+    <Route path="/">
+      <Top />
+    </Route>
   </main>
 </Router>
