9dcedbe10274eba06c05b320372fc6ac1e71bfd9
FujimuraKaito <kaito.bakuman@gmail.com>
2021-12-25 13:42:11 +0900

    ✨ add each grammar

--- svelte-typescript-app/src/App.svelte
+++ svelte-typescript-app/src/App.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import CustomButton from './components/CustomButton.svelte'
+  import type { Menu } from './model/Menu'
 
-  export let name: string
+  let name: string = 'World'
+  let yes: boolean = false
+
+  let flavours: Menu[] = ['Mint choc chip']
+
+  let menu: Menu[] = ['Cookies and cream', 'Mint choc chip', 'Raspberry ripple']
 
   function handleClick() {
     console.log('Button Clicked!', Date.now())
+  }
+
+  function joinFlavour(flavours: Menu[]) {
+    if (flavours.length === 1) return flavours[0]
+    // english notation
+    return `${flavours.slice(0, -1).join(', ')} and ${flavours.at(-1)}`
   }
 </script>
 
@@ -44,6 +56,24 @@
   </p>
   <div class="outerBox">
     <input type="text" bind:value={name} />
+    <label>
+      <input type="checkbox" bind:checked={yes} />
+      Yes! Send me regular email spam
+    </label>
+
+    {#each menu as flavour}
+      <label>
+        <input
+          type="checkbox"
+          bind:group={flavours}
+          name="flavours"
+          value={flavour} />
+        {flavour}
+      </label>
+    {/each}
+
+    {joinFlavour(flavours)}
+
     <CustomButton on:click={handleClick} />
   </div>
 </main>
