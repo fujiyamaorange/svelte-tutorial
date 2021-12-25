37aa8f56a422181a486a88595f1bf792a410264e
FujimuraKaito <kaito.bakuman@gmail.com>
2021-12-25 13:13:39 +0900

    ✨ learn add text binding

--- svelte-typescript-app/src/App.svelte
+++ svelte-typescript-app/src/App.svelte
@@ -1,30 +1,49 @@
 <script lang="ts">
-	export let name: string;
+  import CustomButton from './components/CustomButton.svelte'
+
+  export let name: string
+
+  function handleClick() {
+    console.log('Button Clicked!', Date.now())
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .outerBox {
+    display: flex;
+    flex-direction: column;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <div class="outerBox">
+    <input type="text" bind:value={name} />
+    <CustomButton on:click={handleClick} />
+  </div>
+</main>
