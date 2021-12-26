698c97b993ae6d0da8bb9b5bf8c81cf0189a6600
FujimuraKaito <kaito.bakuman@gmail.com>
2021-12-26 13:59:59 +0900

    ✨ add eliza bot

--- svelte-typescript-app/src/App.svelte
+++ svelte-typescript-app/src/App.svelte
@@ -3,22 +3,24 @@
   import Top from './pages/Top.svelte'
   import Todo from './pages/Todo.svelte'
   import Video from './pages/Video.svelte'
-
-  export let url = ''
+  import Eliza from './pages/Eliza.svelte'
 </script>
 
-<Router {url}>
+<Router>
   <nav>
     <!-- display all time -->
     <Link to="/">home page |</Link>
     <Link to="todo">todo page |</Link>
-    <Link to="video">video page</Link>
+    <Link to="video">video page |</Link>
+    <Link to="top">top page</Link>
   </nav>
   <main>
-    <Route path="todo" component={Todo} />
-    <Route path="video" component={Video} />
-    <Route path="/">
-      <Top />
-    </Route>
+    <div>
+      <Route path="todo" component={Todo} />
+      <Route path="video" component={Video} />
+      <Route path="eliza" component={Eliza} />
+      <Route path="top" component={Top} />
+      <Route exact path="/" component={Eliza} />
+    </div>
   </main>
 </Router>
